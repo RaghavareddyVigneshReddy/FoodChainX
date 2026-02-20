a8e3bc05ad10647581decada1fb03fb677f29026
Vignesh Reddy <2480333@cognizant.com>
2026-02-20 16:20:16 +0530

    Initial Spring Boot project structure with dependencies

---
+++ src/main/java/com/cts/FoodChainX/FoodChainXApplication.java
@@ -0,0 +1,13 @@
+package com.cts.FoodChainX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodChainXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodChainXApplication.class, args);
+	}
+
+}
