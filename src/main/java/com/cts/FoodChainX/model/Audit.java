7e7cd5fc8ddcf0ca67f46021aa10a0cab1325353
R <2480257@cognizant.com>
2026-02-24 18:19:24 +0530

    Resolved merge conflict in Audit model and updated User model

--- src/main/java/com/cts/FoodChainX/model/Audit.java
+++ src/main/java/com/cts/FoodChainX/model/Audit.java
@@ -1,5 +1,10 @@
 package com.cts.FoodChainX.model;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+import org.hibernate.annotations.CreationTimestamp;
 
+<<<<<<< Updated upstream
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,15 +12,22 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+=======
+>>>>>>> Stashed changes
 @Entity
 @Table(name = "AUDIT")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+<<<<<<< Updated upstream
+=======
+@Builder
+>>>>>>> Stashed changes
 public class Audit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+<<<<<<< Updated upstream
     @Column(name = "AuditID")
     private Integer auditID;
 
@@ -35,3 +47,18 @@ public class Audit {
     @Column(name = "Status", length = 50, nullable = false)
     private String status;
 }
+=======
+    private Integer auditId;
+
+    // Mapping as a simple field rather than a Relationship Object
+    @Column(name = "UserID")
+    private Integer userId;
+
+    private String action;
+    private String resource;
+
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+}
+
+>>>>>>> Stashed changes
