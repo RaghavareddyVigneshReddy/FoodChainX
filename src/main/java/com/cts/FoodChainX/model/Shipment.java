37b2048a19c507557bb50351483619ba7d350f61
Joshna <2479352@cognizant.com>
2026-02-25 15:22:55 +0530

    shipment added

--- src/main/java/com/cts/FoodChainX/model/Shipment.java
+++ src/main/java/com/cts/FoodChainX/model/Shipment.java
@@ -1,15 +1,11 @@
 package com.cts.FoodChainX.model;
+
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-<<<<<<< HEAD
-=======
 
 import java.time.LocalDate;
->>>>>>> 1d34a87aa1148910c85de137ade3d1d56eeecd20
-import java.time.LocalDateTime;
 @Entity
 @Table(name="SHIPMENT")
 @Data
@@ -21,19 +17,11 @@ public class Shipment{
     @Column(name="ShipmentID")
     private Integer shipmentID;
     @ManyToOne
-<<<<<<< HEAD
-    @JoinColumn(name="BatchID",nullable="false")
+    @JoinColumn(name="BatchID",nullable=false)
     private ProductionBatch batch;
     @ManyToOne
     @JoinColumn(name="DistributorID",nullable=false)
-    private user distributor;
-=======
-    @JoinColumn(name="BatchID",nullable=false)
-    private Integer batch;
-    @ManyToOne
-    @JoinColumn(name="DistributorID",nullable=false)
     private Integer distributor;
->>>>>>> 1d34a87aa1148910c85de137ade3d1d56eeecd20
     @Column(name="Departuredate",nullable=false)
     private LocalDate departureDate;
     @Column(name="ArrivalDate")
