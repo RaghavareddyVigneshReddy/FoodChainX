cbe140fc6db9362e14a490b8c2d7e9d22aab15a8
Naveen <2479338@cognizant.com>
2026-02-25 16:54:08 +0530

    added files

--- src/main/java/com/cts/FoodChainX/model/Shipment.java
+++ src/main/java/com/cts/FoodChainX/model/Shipment.java
@@ -1,22 +1,23 @@
 package com.cts.FoodChainX.model;
-
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 @Entity
-@Table(name = "SHIPMENT")
+@Table(name="SHIPMENT")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Shipment {
-
+public class Shipment{
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "ShipmentID")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="ShipmentID")
     private Integer shipmentID;
-    
+    @ManyToOne
     @JoinColumn(name="BatchID",nullable=false)
     private Integer batch;
     @ManyToOne
@@ -27,19 +28,9 @@ public class Shipment {
     @Column(name="ArrivalDate")
     private LocalDate arrivalDate;
     @Column(name="Status",length=50)
-
-    @Column(name = "BatchID")
-    private Integer batchID;
-
-    @Column(name = "DistributorID")
-    private Integer distributorID;
-
-    @Column(name = "DepartureDate")
-    private java.sql.Date departureDate;
-
-    @Column(name = "ArrivalDate")
-    private java.sql.Date arrivalDate;
-
-    @Column(name = "Status", length = 50)
     private String status;
+  
+
+    
+
 }
