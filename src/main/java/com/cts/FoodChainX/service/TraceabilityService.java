2e370158149d9f7284505d3e399c762c722c2b8e
Naveen <2479338@cognizant.com>
2026-03-09 09:49:15 +0530

    Added Custom Exception for NOT_FOUND

--- src/main/java/com/cts/FoodChainX/service/TraceabilityService.java
+++ src/main/java/com/cts/FoodChainX/service/TraceabilityService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.cts.FoodChainX.dto.tracerecord.TraceRecordResponseDto;
+import com.cts.FoodChainX.exception.BatchNotFoundException;
 import com.cts.FoodChainX.model.TraceRecord;
 import com.cts.FoodChainX.repository.TraceRecordRepository;
 
@@ -20,19 +21,19 @@ public TraceRecordResponseDto getTraceabilityData(Long batchId) {
     // Change _BatchId to _ProductionId to match your Repository
     return traceRecordRepository.findByProductionBatch_ProductionId(batchId)
             .map(this::mapToResponse)
-            .orElseThrow(() -> new EntityNotFoundException("Trace history for Batch ID " + batchId + " not found."));
+            .orElseThrow(() -> new BatchNotFoundException(batchId));
 }
 
 private TraceRecordResponseDto mapToResponse(TraceRecord record) {
     return TraceRecordResponseDto.builder()
             .traceId(record.getTraceId())
             .batchId(record.getProductionBatch().getProductionId())
-            // ✅ Fetch Crop Type from the Production table
+            // Fetch Crop Type from the Production table
             .cropType(record.getProductionBatch().getCropType())
-            // ✅ Fetch Farm Name from the Farm table
+            // Fetch Farm Name from the Farm table
             .farmName(record.getFarm() != null ? record.getFarm().getName() : "N/A")
             .distributorName(record.getDistributor() != null ? record.getDistributor().getLocation() : "In Transit")
-            // ✅ Fetch Consumer Name from the User relationship
+            // Fetch Consumer Name from the User relationship
             .consumerName(record.getConsumer() != null ? record.getConsumer().getName() : "Not Yet Purchased")
             .status(record.getStatus())
             .date(record.getDate())
