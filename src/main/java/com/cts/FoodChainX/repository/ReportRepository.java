c237e0d887dc31b4e846a21c4f362e29512eaf53
Naveen <2479338@cognizant.com>
2026-03-09 15:01:22 +0530

    Created Controller for Report

--- src/main/java/com/cts/FoodChainX/repository/ReportRepository.java
+++ src/main/java/com/cts/FoodChainX/repository/ReportRepository.java
@@ -1,8 +1,11 @@
 package com.cts.FoodChainX.repository;
 
+import com.cts.FoodChainX.model.Report; // Ensure you have a Report entity matching source 92
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.cts.FoodChainX.model.Report;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
-public interface ReportRepository extends JpaRepository<Report, Integer> {
-    
+@Repository
+public interface ReportRepository extends JpaRepository<Report, Long> {
+    List<Report> findByScopeIgnoreCase(String scope);
 }
