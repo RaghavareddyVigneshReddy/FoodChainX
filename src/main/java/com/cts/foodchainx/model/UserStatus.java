df3c8afcf1849631fcc844f973e276498dde3637
Joshna <2479352@cognizant.com>
2026-03-16 08:47:01 +0530

    logistics Service added

--- src/main/java/com/cts/foodchainx/model/UserStatus.java
+++ src/main/java/com/cts/foodchainx/model/UserStatus.java
@@ -1,13 +1,6 @@
 package com.cts.foodchainx.model;
 
 
-<<<<<<< HEAD
-public enum UserStatus {
-  ACTIVE,
-  INACTIVE,
-  SUSPENDED
-}
-=======
 /**
  * Represents the administrative status of a {@link User} account.
  */
@@ -16,4 +9,3 @@ public enum UserStatus {
   INACTIVE,  // Account created but not yet verified or active
   SUSPENDED  // Account blocked by Admin due to policy violations
 }
->>>>>>> d067e7d9657dbb3bba899c6df80c3f723990653e
